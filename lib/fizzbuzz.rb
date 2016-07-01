7b1fb169a859c327f0d4e9653acb81eefda42c35
Ben Ross <benjaminjackross@gmail.com>
2016-07-01 14:22:15 +0000

    Refactored fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,6 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-        'fizzbuzz'
-    elsif number % 3 == 0
-        'fizz'
-    elsif number % 5 == 0
-        'buzz'
+    if (number % 3 == 0) && (number % 5 == 0) then return 'fizzbuzz'
+    elsif number % 3 == 0 then return 'fizz'
+    elsif number % 5 == 0 then return 'buzz'
     end
 end
